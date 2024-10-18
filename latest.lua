29c5a622cb29c36b1c8f2354e8a07f1f3218b1fe
kz0x1 <121727107+kz0x1@users.noreply.github.com>
2024-10-18 18:37:26 +0530

    Update latest.lua

--- latest.lua
+++ latest.lua
@@ -4,6 +4,6 @@ local StarterGui = game:GetService("StarterGui")
 StarterGui:SetCore("SendNotification", {
     Title = "Raz!";
     Text = "Thanks to Raz on discord for helping me add unc. <3";
-    Icon = ""; -- Optional, you can put an image asset ID here.
-    Duration = 7; -- Time in seconds the notification will be displayed.
+    Icon = "";
+    Duration = 7;
 })
