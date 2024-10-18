1e2e2b2ed5feb700e416defe90190cb92f1e7209
kz0x1 <121727107+kz0x1@users.noreply.github.com>
2024-10-18 18:34:39 +0530

    Update latest.lua

--- latest.lua
+++ latest.lua
@@ -1,1 +1,9 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/kz0x1/nsploit-loadstrings/refs/heads/main/77_AENKL.txt"))()
+local StarterGui = game:GetService("StarterGui")
+
+StarterGui:SetCore("SendNotification", {
+    Title = "Raz!";
+    Text = "Thanks to Raz on discord for helping me add unc. <3";
+    Icon = ""; -- Optional, you can put an image asset ID here.
+    Duration = 7; -- Time in seconds the notification will be displayed.
+})
